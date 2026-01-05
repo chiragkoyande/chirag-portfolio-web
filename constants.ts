f5c10367332780f16e1fdd7021d2376489728fef
chirag koyande <chiragkoyande4@gmail.com>
2026-01-05 20:27:40 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -35,7 +35,7 @@ export const PROJECTS_DATA: Project[] = [
     securityLevel: 'CRITICAL',
     status: 'ANALYZING',
     links: {
-      github: 'https://github.com/chiragkoyande/linksniff-phishing-URL-detection-in-pdf-',
+      github: 'https://github.com/chiragkoyande/linksniff-phishing-URL-detection-in-pdf',
       demo: 'https://linksniff-phishing-url-detection-in-pdf.onrender.com/'
     }
   },
